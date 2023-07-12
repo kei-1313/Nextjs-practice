48f75f70e32ba8ec62d5c4d5143bc66a8dfcadc9
kei-1313 <candydog666@gmail.com>
2023-07-13 02:19:25 +0900

    useRouterを使った場合

--- src/pages/blog/[id].js
+++ src/pages/blog/[id].js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useRouter } from 'next/router';
 
 const id = ({query}) => {
+  const router = useRouter();
+  console.log(router);
   return (
     <div>
       <h1>動的です</h1>
-      <h2>{ query.id }</h2>
+      <h2>{ router.query.id }</h2>
     </div>
   )
 }
