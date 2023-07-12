6b95f81f574c0fa8a4905da6e8ef7587a0f327df
kei-1313 <candydog666@gmail.com>
2023-07-13 02:17:20 +0900

    クエリの取得の仕方

--- src/pages/blog/[id].js
+++ src/pages/blog/[id].js
@@ -1,11 +1,19 @@
 import React from 'react'
 
-const id = () => {
+const id = ({query}) => {
   return (
     <div>
       <h1>動的です</h1>
+      <h2>{ query.id }</h2>
     </div>
   )
+}
+
+export async function getServerSideProps({ query }) {
+  console.log(query);
+  return {
+    props: { query }
+  }
 }
 
 export default id
