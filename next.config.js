7ff13ec350706b5fa3f2052507ae603664bf5795
kei-1313 <candydog666@gmail.com>
2023-07-16 12:27:04 +0900

    rewritesの設定記述

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/personal',
+        destination: '/multiple'
+      },
+      {
+        source: '/confirm',
+        destination: '/multiple'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
