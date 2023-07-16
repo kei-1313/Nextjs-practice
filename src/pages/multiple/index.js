cde2042c8bef6c2b500000e45aafd5367c2837d8
kei-1313 <candydog666@gmail.com>
2023-07-16 12:23:09 +0900

    クエリパラメータを取得してページを変更する

--- src/pages/multiple/index.js
+++ src/pages/multiple/index.js
@@ -3,15 +3,30 @@ import { useRouter } from "next/router";
 const multiplePage = () => {
   const router = useRouter();
   const step = router.query.step ?? 0
+
+  const goToStep = (_step, asPath) => {
+    router.push(`/multiple?step=${_step}`, asPath)
+  }
   return (
     <div>
-      {step == 0 && (
+      { step == 0 && (
         <>
           <h3>Step {step}</h3>
           <buttton onClick={() => goToStep(1, "/personal")}>Next Step</buttton>
         </>
-      )
-      }
+      )}
+      { step == 1 && (
+        <>
+          <h3>Step {step}</h3>
+          <buttton onClick={() => goToStep(2, "/confirm")}>Next Step</buttton>
+        </>
+      )}
+      { step == 2 && (
+        <>
+          <h3>Step {step}</h3>
+          <buttton onClick={() => goToStep(0, "/multiple")}>Next Step</buttton>
+        </>
+      )}
     </div>
   )
 }
