0d4310e22f9805312ef7578cf500dc98b692c7d3
kei-1313 <candydog666@gmail.com>
2023-07-17 11:17:27 +0900

    getStaticPropsの使い方

--- src/pages/sg/index.js
+++ src/pages/sg/index.js
@@ -1,9 +1,16 @@
-function Sg() {
+function Sg({message}) {
   return (
     <div>
       <h1>SGページ</h1>
+      <h2>{message}</h2>
     </div>
   )
 }
 
 export default Sg
+
+export async function getStaticProps() {
+  return {
+    props: {message: 'From Static Props'}
+  }
+}
